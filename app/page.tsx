d9d79fda10184fa4724952496d6532194d87ba1d
pidg3 <michael.r.pidgeon@gmail.com>
2023-08-17 11:20:44 +0100

    Change text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
+          Some random edits
+          Blah blah blah
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
